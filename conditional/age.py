98dca9f99cdeac29bfb2d63dd7132fe6a9c24837
Muskanyadav29 <ydvmuskan29@gmail.com>
2025-07-14 15:09:59 +0530

    age.py

---
+++ conditional/age.py
@@ -0,0 +1,9 @@
+age=int(input("enter age:"))
+if(age<12):
+    print("you r a child")
+elif(age>=12 and age<20):
+    print("you r a teenager")
+elif(age>=20 and age<50):
+    print("you r an adult")
+else:
+    print("you r a senior")
